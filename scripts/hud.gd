7c77b67678846efbb685180e4112b17b5ed74d84
Amaar <amaar.khan02@gmail.com>
2026-01-22 00:36:59 +0000

    Add nicer play/pause button and remove unneeded assets

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -5,9 +5,21 @@ signal pause
 
 @onready var texture_button: TextureButton = $TextureButton
 
+var paused := true
 
-func _on_texture_button_toggled(toggled_on: bool) -> void:
-	if toggled_on:
+var play_texture_normal = preload("res://assets/play_button_normal.png")
+var play_texture_pressed = preload("res://assets/play_button_pressed.png")
+var pause_texture_normal = preload("res://assets/pause_button_normal.png")
+var pause_texture_pressed = preload("res://assets/pause_button_pressed.png")
+
+func _on_texture_button_pressed() -> void:
+	if paused:
 		play.emit()
+		paused = false
+		texture_button.texture_normal = pause_texture_normal
+		texture_button.texture_pressed = pause_texture_pressed
 	else:
 		pause.emit()
+		paused = true
+		texture_button.texture_normal = play_texture_normal
+		texture_button.texture_pressed = play_texture_pressed
