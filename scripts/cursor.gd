837390579492afcb4f61bdf221d6cda932047cb0
Amaar <amaar.khan02@gmail.com>
2026-01-23 21:32:01 +0000

    Add cell cursor

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -1,11 +1,11 @@
 extends TileMapLayer
 
-@export var grid: TileMapLayer
+var prev_mouse_pos: Vector2i
 
 func _process(delta: float) -> void:
 	var tile := local_to_map(get_global_mouse_position())
-	var index = grid.util.vector_to_index(tile)
+	set_cell(tile, 0, Vector2(0,0), 0)
 	
-	print(index)
-	if index > 0 && index < grid.cells.size():
-		set_cell(tile, 1, Vector2(0,0), 0)
+	if prev_mouse_pos != tile:
+		erase_cell(prev_mouse_pos)
+		prev_mouse_pos = tile
