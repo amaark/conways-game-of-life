ae5a77f0a5a53451e3fb9f08e67a2ba6f2a5dd15
Amaar <amaar.khan02@gmail.com>
2026-02-08 16:59:22 +0000

    Fill in gaps when painting cells with linear interpolation

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -2,10 +2,11 @@ extends TileMapLayer
 
 var prev_mouse_pos: Vector2i
 
+
 func _process(delta: float) -> void:
-	var tile := local_to_map(get_global_mouse_position())
-	set_cell(tile, 0, Vector2(0,0), 0)
-	
+	var tile := local_to_map(get_local_mouse_position())
+	set_cell(tile, 0, Vector2(0, 0), 0)
+
 	if prev_mouse_pos != tile:
 		erase_cell(prev_mouse_pos)
 		prev_mouse_pos = tile
--- scripts/main.gd
+++ scripts/main.gd
@@ -7,8 +7,8 @@ var live_cells: Dictionary[Vector2i, bool] = {
 	Vector2i(19, 10): true,
 	Vector2i(20, 10): true,
 	Vector2i(20, 9): true,
-	Vector2i(19, 8): true
-	}
+	Vector2i(19, 8): true,
+}
 
 var neighbours := [
 	Vector2i(-1, -1),
@@ -19,7 +19,7 @@ var neighbours := [
 	Vector2i(1, -1),
 	Vector2i(1, 0),
 	Vector2i(1, 1),
-	]
+]
 
 var prev_mouse_pos
 
@@ -31,9 +31,11 @@ var time_elapsed := 0.0
 
 var paused := true
 
+
 func _ready() -> void:
 	for coords in live_cells:
 		grid.set_cell_state(coords, true)
+
 
 func _physics_process(delta: float) -> void:
 	if not paused:
@@ -41,38 +43,51 @@ func _physics_process(delta: float) -> void:
 		if time_elapsed >= wait_time:
 			evolve()
 			time_elapsed = 0.0
-	
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_pressed("paint_cell"):
 		click()
 	if Input.is_action_just_released("paint_cell"):
 		prev_mouse_pos = null
 
+
 func click() -> void:
 	if Input.is_action_just_pressed("paint_cell"):
-		painting_mode = not grid.local_to_map(get_global_mouse_position()) in live_cells
-	
-	var coords := grid.local_to_map(get_global_mouse_position())
-	
+		painting_mode = not grid.local_to_map(get_local_mouse_position()) in live_cells
+
+	var curr_mouse_pos := grid.local_to_map(get_local_mouse_position())
+
 	# Debounce
-	if coords == prev_mouse_pos:
+	if curr_mouse_pos == prev_mouse_pos:
 		return
-	
-	prev_mouse_pos = coords
-	grid.set_cell_state(coords, painting_mode)
-	
+
+	# Fill in gaps between this frame and the previous with linear interpolation
+	if prev_mouse_pos != null:
+		var distance: Vector2i = curr_mouse_pos - prev_mouse_pos
+		for i in distance.length():
+			var cell := Vector2i(Vector2(curr_mouse_pos).lerp(prev_mouse_pos, i / distance.length()))
+			grid.set_cell_state(cell, painting_mode)
+			if painting_mode:
+				live_cells[cell] = true
+			else:
+				live_cells.erase(cell)
+
+	prev_mouse_pos = curr_mouse_pos
+	grid.set_cell_state(curr_mouse_pos, painting_mode)
 	if painting_mode:
-		live_cells[coords] = true
+		live_cells[curr_mouse_pos] = true
 	else:
-		live_cells.erase(coords)
+		live_cells.erase(curr_mouse_pos)
+
 
 ## Flips live cells and their neighbours according to the game rules
 func evolve() -> void:
-	var num_neighbours: Dictionary[Vector2i, int] = {}
+	var num_neighbours: Dictionary[Vector2i, int] = { }
 	var to_live: Array[Vector2i] = []
 	var to_die: Array[Vector2i] = []
 	var neighbour_coords: Vector2i
-	
+
 	# Increment the number of neighbours for each live cell's neighbours
 	for cell in live_cells:
 		# Make sure that solitary cells still get added to num_neighbours
@@ -84,7 +99,7 @@ func evolve() -> void:
 				num_neighbours[neighbour_coords] += 1
 			else:
 				num_neighbours[neighbour_coords] = 1
-	
+
 	# Check each live cell and adjacent cell against the 4 cell rules
 	for coords in num_neighbours:
 		if (num_neighbours[coords] < 2 or num_neighbours[coords] > 3) and coords in live_cells:
@@ -94,15 +109,19 @@ func evolve() -> void:
 			grid.set_cell_state(coords, true)
 			live_cells[coords] = true
 
+
 func _on_hud_play() -> void:
 	paused = false
+
 
 func _on_hud_pause() -> void:
 	paused = true
 	time_elapsed = 0.0
 
+
 func _on_hud_change_speed(new_value: float) -> void:
 	wait_time = WAIT_TIME_BASE / new_value
+
 
 func _on_hud_clear() -> void:
 	for cell in live_cells:
